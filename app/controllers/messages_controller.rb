e3ac07a805048e56f9c3c593951dd374e9b1812a
Cory Schires <coryschires@gmail.com>
2010-08-07 00:44:31 -0500

    Changed redirect in messages create action.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -44,7 +44,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to(@message, :notice => 'Message was successfully created.') }
+        format.html { redirect_to messages_path }
         format.xml  { render :xml => @message, :status => :created, :location => @message }
       else
         format.html { render :action => "new" }
