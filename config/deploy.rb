5378d7b9e19f9dc6969edbf3badbac9fe9a32d04
Cory Schires <coryschires@gmail.com>
2010-08-06 23:27:38 -0500

    Changes to deploy.rb file

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,19 +32,3 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
-
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(current_release, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
- 
-  task :bundle_new_release, :roles => :app do
-    bundler.create_symlink
-    run "cd #{release_path} && bundle install --without test development"
-  end
-end
-
-
-after 'deploy:update_code', 'bundler:bundle_new_release'
